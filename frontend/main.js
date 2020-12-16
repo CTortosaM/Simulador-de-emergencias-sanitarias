b67440f909bc008f2bcc87624a66b6caf1061176
CTortosaM <cartormi@epsg.upv.es>
2020-12-16 20:01:15 +0100

    Init

---
+++ frontend/main.js
@@ -0,0 +1,47 @@
+const coordsUniversitat = {
+    lng: 38.955,
+    lat: -0.1655,
+    zoom: 15
+}
+
+// LEAFLET
+const elMapa = L.map('mapa');
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(elMapa);
+
+elMapa.on('click', (event => {
+    let coord = event.latlng;
+    obtenerGeoJson(coord.lat, coord.lng)
+}));
+// ---------------
+
+async function obtenerCoordsEPSG() {
+    let coords = await eel.obtenerCoordsEPSG()();
+    coords = JSON.parse(coords);
+    console.log(coords);
+    elMapa.setView([coords.long, coords.lat], 15);
+}
+
+async function obtenerGeoJson(lng, lat) {
+
+    // Elimina las capas
+     elMapa.eachLayer((capa) => {
+        elMapa.removeLayer(capa);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(elMapa);
+    });
+
+    let geojson = await eel.obtenerGeoJson(lng, lat)();
+
+    L.geoJSON(geojson).addTo(elMapa)
+}
+
+
+obtenerCoordsEPSG();
+obtenerGeoJson(coordsUniversitat.lng, coordsUniversitat.lat);
+
+// Bloquejat per problemes en CORS
+//obtenerGeoJson();
---
+++ main.py
@@ -0,0 +1,30 @@
+import eel
+import json
+import requests
+
+# Exposa la funció al script JS
+@eel.expose
+def obtenerCoordsEPSG():
+    coords = {
+        "long": 38.955998,
+        "lat": -0.165564,
+    }
+
+    # Convertir en json string
+    coordenadas = json.dumps(coords)
+    return coordenadas
+
+@eel.expose
+def obtenerGeoJson(lng, lat):
+    body = {"locations":[[lat, lng]],"range":[900]}
+
+    headers = {
+        'Accept': 'application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8',
+        'Authorization': '5b3ce3597851110001cf6248d90c17ea66a34789876d047afe84449e',
+        'Content-Type': 'application/json; charset=utf-8'
+    }
+    call = requests.post('https://api.openrouteservice.org/v2/isochrones/driving-car', json=body, headers=headers)
+    return call.json()
+
+eel.init("frontend")
+eel.start('main.html', cmdline_args=['--start-fullscreen'], size=(1280, 720), position=(0,0))
